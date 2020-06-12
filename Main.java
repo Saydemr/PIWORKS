fe1fd6242e37757cd7324dc9190e21463ae91596
Abdullah Saydemir <37012364+Saydemr@users.noreply.github.com>
2020-06-12 16:18:19 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,47 @@
+/*
+    ABDULLAH SAYDEMIR
+    LONGER PATH > BIGGER SUM
+
+
+ */
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            ArrayList<String> lines = new ArrayList<>();
+            Scanner console = new Scanner(System.in);
+
+            while (console.hasNextLine()) {
+                String line = console.nextLine();
+                lines.add(line);
+            }
+
+            int[][] arr = new int[lines.size()][lines.size()];
+
+            for (int i = 0; i < lines.size(); i++) {
+
+                String[] strings = lines.get(i).split(" ");
+
+                for (int j = 0; j < strings.length; j++) {
+                    arr[i][j] = Integer.parseInt(strings[j]);
+                }
+            }
+
+            PathFinder pathFinder = new PathFinder(arr, 0, 0);
+            int result = pathFinder.maxSum;
+
+            System.out.println("Result : " + result);
+            System.out.println();
+
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Please give the input as a pyramid.");
+            main(args);
+        }
+
+    }
+}
+
+
---
+++ PathFinder.java
@@ -0,0 +1,76 @@
+public class PathFinder {
+
+    int reach;
+    int[][] integerArray;
+    int x, y;
+    int maxSum;
+
+    public PathFinder(int[][] integerArray,int x, int y) {
+        this.integerArray = integerArray;
+        this.reach = x;
+        this.x = x;
+        this.y = y;
+        this.maxSum = percolate(integerArray,x,y);
+    }
+
+    int percolate(int[][] integerArray,int i, int j) {
+
+          if (i == integerArray.length - 1) {
+              this.reach = integerArray.length;
+              return integerArray[i][j]; // if the position is at the end return the value in this position
+          }
+          else {
+              PathFinder path1 = new PathFinder(integerArray,i+1,j);
+              PathFinder path2 = new PathFinder(integerArray,i+1,j+1);
+              if (primeCheck(integerArray[i + 1][j]) && primeCheck(integerArray[i + 1][j + 1])) {
+                this.reach++;
+                return integerArray[i][j];            // if both left and right positions have prime numbers stop here
+              }
+              else if (primeCheck(integerArray[i + 1][j])) {
+                  this.reach += path2.reach + 1;
+                  return integerArray[i][j] + path2.maxSum;   // if left is prime go right
+              }
+              else if (primeCheck(integerArray[i + 1][j + 1])) {
+                  this.reach += path1.reach +1;
+                  return integerArray[i][j] + path1.maxSum;   // if right is prime go left
+              }
+              else {                                                                  // if both not prime
+                  if (path1.reach == path2.reach) { // lengths are the same chose max
+                      this.reach += path1.reach + 1;
+                      return integerArray[i][j] + Math.max(path1.maxSum, path2.maxSum);
+                  }
+                  else if (path1.reach < path2.reach) { // if path2 longer chose it
+                      this.reach += path2.reach + 1;
+                      return integerArray[i][j] + path2.maxSum;
+                  }
+                  else {                               // if path1 longer chose it
+                      this.reach += path1.reach +1;
+                      return integerArray[i][j] + path1.maxSum;
+                  }
+
+              }
+
+          }
+    }
+
+    boolean primeCheck(int a) {
+
+        if ( a < 2)
+            return false;
+
+        for (int i = 2 ; i <= a/2 ; i++) {
+            if (a % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    void arrPrint(int[][] arr) {
+        for (int i = 0; i < integerArray.length ; i++) {
+            System.out.println();
+            for (int j = 0 ; j < integerArray[0].length ; j++)
+                System.out.print(arr[i][j] + " ");
+        }
+    }
+}
+
